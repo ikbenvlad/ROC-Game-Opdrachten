395d611208c9740976c4060ace2a20873768583c
Vlad <vladalexandru.tarba@gmail.com>
2025-02-06 13:51:55 +0100

    Volledige dingetje

---
+++ ConsoleApp2/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Calculator
+    {
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Min(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+        public int Mult(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Mod(int a, int b)
+        {
+            return a % b;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+
+        static void Main(string[] args)
+        {
+            //Begint hier
+            //plus, min, delen door en bonus: Modulo
+            //een instance maken van de calculator
+            Calculator mijnCalculator = new Calculator();
+            int antwoord = mijnCalculator.Plus(10, 3);
+            int antwoord1 = mijnCalculator.Min(10, 3);
+            int antwoord2 = mijnCalculator.Div(10, 3);
+            int antwoord3 = mijnCalculator.Mult(10, 3);
+            int antwoord4 = 100;
+            Console.WriteLine(antwoord);
+            Console.WriteLine(antwoord1);
+            Console.WriteLine(antwoord2);
+            Console.WriteLine(antwoord3);
+            
+            Console.WriteLine(antwoord4);
+
+            //maak een output van 0 t/m 100
+            for (int i = 0; i <= antwoord4; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("XD");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+}
+
+/* 
+ QUIZ-OPDRACHTEN LES
+
+ Hoe herken je een functie?
+ - het staat binnen de haakjes '()' en bevat een scope '{}'
+
+ Hoe herken je een varabele?
+ - het heeft geen haakjes of scope
+
+ Hoe zou je omschrijven wat een class is?
+ - een soort van blueprint die je ten alle tijden kan gebruiken
+ */
